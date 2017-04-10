986a55eb384cf1a0bd3ac35550d2ae3f241a5351
Igor Amidzic <amidzicigor@yahoo.com>
2017-04-09 21:27:22 -0400

    Fix bootstrap.css link

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Stylesheets -->
     <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="bower_components/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/all.css">
     <!-- Title -->
     <title>Igor Amidzic - Web Developer</title>
