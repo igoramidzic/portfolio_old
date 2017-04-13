e1c65cc327944ef5f862439d140c88a1cfeb56ed
Igor Amidzic <amidzicigor@yahoo.com>
2017-04-13 10:03:38 -0400

    Add projects section

--- resources/views/partials/main-projects.blade.php
+++ resources/views/partials/main-projects.blade.php
@@ -9,6 +9,33 @@
       <div class="title-bar">
         <div class="bar-accent"></div>
       </div>
+
+      <div class="projects">
+        <a class="project-thumb" style="background-image: url(img/kodekademy-project.png)" target="_blank" href="http://www.kk.igoramidzic.com">
+          <div class="project-meta">
+            <h1 class="title">Codecademy Replica</h1>
+            <p class="description">Made with NodeJS, Express, and MongoDB</p>
+          </div>
+        </a><!--
+        --><a class="project-thumb img-responsive" style="background-image: url(img/sipnub-project.png)" target="_blank" href="http://www.sn.igoramidzic.com">
+          <div class="project-meta">
+            <h1 class="title">Github Replica</h1>
+            <p class="description">Made with PHP and MySQL</p>
+          </div>
+        </a><!--
+        --><a class="project-thumb img-responsive" style="background-image: url(img/freenters.png)" target="_blank" href="http://www.freenters.com">
+          <div class="project-meta">
+            <h1 class="title">Freenters Internship</h1>
+            <p class="description">Company I worked for as a web developer intern</p>
+          </div>
+        </a><!--
+        --><a class="project-thumb img-responsive" style="background-image: url(img/weather-project.png)" target="_blank" href="http://www.wa.igoramidzic.com">
+          <div class="project-meta">
+            <h1 class="title">Weather App</h1>
+            <p class="description">A basic weather app built using Google APIs</p>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
