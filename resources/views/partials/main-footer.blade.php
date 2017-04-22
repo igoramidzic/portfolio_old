310c76727bb33ae0e3a76e4890bf11b39fd34f9a
Igor Amidzic <amidzicigor@yahoo.com>
2017-04-22 10:46:54 -0400

    Reorder footer links

--- resources/views/partials/main-footer.blade.php
+++ resources/views/partials/main-footer.blade.php
@@ -5,8 +5,8 @@
     <p class="description">Designed and developed by <br class="hidden-lg hidden-md hidden-sm"> Igor Amidzic <span>|</span> 2017</p>
     <ul class="links">
       <li data-aos="flip-left" data-aos-delay="100" data-aos-duration="1000" class="item"><a class="link" target="_blank" href="https://www.facebook.com/igor.amidzic14"><i class="fa fa-facebook-square"></i></a></li><!--
-      --><li data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" class="item"><a class="link" target="_blank" href="https://github.com/igoramidzic"><i class="fa fa-github-square"></i></a></li><!--
-      --><li data-aos="flip-left" data-aos-delay="300" data-aos-duration="1000" class="item"><a class="link" href="mailto:amidzicigor@yahoo.com"><i class="fa fa-envelope"></i></a></li>
+      --><li data-aos="flip-left" data-aos-delay="300" data-aos-duration="1000" class="item"><a class="link" href="mailto:amidzicigor@yahoo.com"><i class="fa fa-envelope"></i></a></li><!--
+      --><li data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" class="item"><a class="link" target="_blank" href="https://github.com/igoramidzic"><i class="fa fa-github-square"></i></a></li>
     </ul>
   </div>
 </section>
