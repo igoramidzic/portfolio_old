12677c6c9dcebae0cc6750c408070ef9a45a423f
Igor Amidzic <amidzicigor@yahoo.com>
2017-04-25 16:31:16 -0400

    Add aos-anchor for footer links

--- resources/views/partials/main-footer.blade.php
+++ resources/views/partials/main-footer.blade.php
@@ -1,12 +1,12 @@
 <!-- Main footer -->
 <section class="main-footer">
   <div class="content container">
-    <h1 class="logo">&lt; IA /&gt;</h1>
+    <h1 class="logo links-anchor">&lt; IA /&gt;</h1>
     <p class="description">Designed and developed by <br class="hidden-lg hidden-md hidden-sm"> Igor Amidzic <span>|</span> 2017</p>
     <ul class="links">
-      <li data-aos="flip-left" data-aos-delay="100" data-aos-duration="1000" class="item"><a class="link" target="_blank" href="https://www.facebook.com/igor.amidzic14"><i class="fa fa-facebook-square"></i></a></li><!--
-      --><li data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" class="item"><a class="link" target="_blank" href="https://github.com/igoramidzic"><i class="fa fa-github-square"></i></a></li><!--
-      --><li data-aos="flip-left" data-aos-delay="300" data-aos-duration="1000" class="item"><a class="link" href="mailto:amidzicigor@yahoo.com"><i class="fa fa-envelope"></i></a></li>
+      <li data-aos="flip-left" data-aos-delay="100" data-aos-duration="1000" data-aos-anchor=".links-anchor" class="item"><a class="link" target="_blank" href="https://www.facebook.com/igor.amidzic14"><i class="fa fa-facebook-square"></i></a></li><!--
+      --><li data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" data-aos-anchor=".links-anchor" class="item"><a class="link" target="_blank" href="https://github.com/igoramidzic"><i class="fa fa-github-square"></i></a></li><!--
+      --><li data-aos="flip-left" data-aos-delay="300" data-aos-duration="1000" data-aos-anchor=".links-anchor" class="item"><a class="link" href="mailto:amidzicigor@yahoo.com"><i class="fa fa-envelope"></i></a></li>
     </ul>
   </div>
 </section>
